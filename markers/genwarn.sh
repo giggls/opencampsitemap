91d972cc15a6b3fc72eaa615e62df160034ca7c2
Sven Geggus <sven-git@geggus.net>
2024-10-03 17:12:27 +0200

    Add automatic generation of the following svg files:
    m_private_*
    l_private_*
    
    m_*_warn.svg
    m_*_warn_sel.svg

--- markers/genwarn.sh
+++ markers/genwarn.sh
@@ -3,8 +3,6 @@
 IFS="
 "
 
-WARN=warn.svg
+path=$(xmlstarlet sel -N ns=http://www.w3.org/2000/svg -t -c "//ns:path" $1 |sed -e 's/^<//' -e 's;/>$;;')
 
-path=$(xmlstarlet sel -N ns=http://www.w3.org/2000/svg -t -c "//ns:path" $WARN |sed -e 's/^<//' -e 's;/>$;;')
-
-xmlstarlet ed -N ns=http://www.w3.org/2000/svg --subnode "//ns:svg" --type elem -n $path $1
+xmlstarlet ed -N ns=http://www.w3.org/2000/svg --subnode "//ns:svg" --type elem -n $path $2
